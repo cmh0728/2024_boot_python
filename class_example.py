82b421d50753229cb2cad578c7f1b5eafabba879
cmhboot_camp <gambaro344@gmail.com>
2024-01-15 13:45:08 +0900

    example2

---
+++ class_example.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+#class_example
+
+class bk_joon : 
+    def __init__(self):
+        pass
+
+    def add_func(self,a,b):
+        rst = a+b
+        return rst
+
+def main():
+    try:
+        a,b = map(int,input().split())
+        func = bk_joon()
+        rst = func.add_func(a,b)
+        print(rst)
+    except: print('error')
+
+
+if __name__ == "__main__":
+    main()
