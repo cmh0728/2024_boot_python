364aa3fe7a6f8b5bc3e06cfd2c357f57572b352a
cmhboot_camp <gambaro344@gmail.com>
2024-01-16 15:36:45 +0900

    prime_number option

--- day03.py
+++ day03.py
@@ -15,19 +15,38 @@ class boot_camp:
                     return 0
             else : 
                 return 1
-            
+    
+    def prime_numbers_list(self,a,b):
+        arr = []
+        for i in range(a,b+1):
+            rst = self.prime_number(i)
+            if rst == 0 : #소수가 아닐때 
+                continue
+            elif rst == 1 : #소수일때 
+                arr.append(i)
+        return arr
     
 def main():
     try:
-        number = int(input("Input number : "))
-        class_name = boot_camp()
-        rst = class_name.prime_number(number)
-        if rst == 1:
-            print(f'{number} is prime number')
-        elif rst == 0 :
-            print(f'{number} is "not" prime number')
-
-        
+        option = int(input("select option 1)'check prime number' 2)prime number between two number : "))
+        if option == 1:
+            number = int(input("Input number : "))
+            class_name = boot_camp()
+            rst = class_name.prime_number(number)
+            if rst == 1: #1반환하면 소수
+                print(f'{number} is prime number')
+            elif rst == 0 : #아니면 소수x
+                print(f'{number} is "not" prime number')
+        elif option == 2:
+            first_number = int(input("first number : "))
+            second_number = int(input("second number : "))
+            class_name = boot_camp()
+            rst_array = class_name.prime_numbers_list(first_number,second_number)
+            print("The prime numbers : ",end=' ')
+            print('"',end=' ')
+            for i in range(len(rst_array)):
+                print(rst_array[i],end=' ')
+            print('"',end='')
     except: 
         print("error")
 
